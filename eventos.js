76ebdb18221a09188efb0d73d7473f20cb55ff08
Dana959 <danaamachado@gmail.com>
2024-08-13 19:52:37 -0300

    Agregue evento al boton en JS, aún permanece en atributo HTML

--- eventos.js
+++ eventos.js
@@ -1,8 +1,19 @@
 document.addEventListener('DOMContentLoaded', function () {
 
 const div = document.getElementById(`container`);
+const boton = document.getElementById(`btn`);
+
+// Agrego manejador de eventos al boton para poder detener la propagación del mismo. 
+
+boton.addEventListener(`click`, function(event){
+    alert(`Hola!`);
+    event.stopPropagation(); // Detiene la propagación
     
 div.addEventListener('click', function () {
         alert('Hola! Soy el div');
+
     });
+
+    });
+    
 });
